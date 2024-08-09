e6002652fae50ab71dfb28d9c60918c720a1b112
Jefferson Alves <jefff9910@outlook.com>
2024-08-08 21:20:20 -0300

    Inicio de aulas

---
+++ iniciando_kotlin/src/Main.kt
@@ -0,0 +1,19 @@
+
+
+
+fun main() {
+    var nome: String = "Jefferson"
+    val nome01 = "Jose"
+    println("Ola Mundo")
+
+    fun soma(a: Int, b: Int): Int {
+        return a + b
+    }
+
+    fun chamaPalavra(palavra : String) {
+        println(palavra.length)
+
+    }
+
+    println(soma(2, 1))
+}
